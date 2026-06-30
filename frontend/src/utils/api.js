73f932bc79dcdbe18f4da745508d07217f9943da
Flux <focusanshu77@gmail.com>
2026-07-01 00:49:58 +0530

    baseurl change for deployment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  // Looks for Vercel's environment variable first; falls back to localhost if running locally
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 });
 
 // Automatically add token to every request
